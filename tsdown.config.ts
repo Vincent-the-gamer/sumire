808468306885399ba52204359f61ff2ea8d669e5
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-11-26 15:22:31 +0800

    feat: support both ESM and CommonJS

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     './src/index.ts',
   ],
   clean: true,
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   dts: true,
   minify: true,
   // compatible with __dirname in cjs and import.meta.url in mjs.
