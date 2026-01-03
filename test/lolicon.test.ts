017bcdafcb82784cca8873a48c8f2c03ad441023
Vincent-the-gamer <erxianqiaottjt@163.com>
2026-01-03 14:39:37 +0800

    chore: update deps.

--- test/lolicon.test.ts
+++ test/lolicon.test.ts
@@ -5,7 +5,13 @@ async function testLolicon() {
     r18: 1,
     num: 5,
     tag: [
-      ['萝莉', '少女']
+      ['萝莉', '少女'],
     ],
+    aspectRatio: 'gte1.777lte1.778',
   })
 }
+
+const a = await testLolicon()
+console.log(
+  JSON.stringify(a),
+)
